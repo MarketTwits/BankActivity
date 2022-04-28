d5607ad16c4ee9c873de3a32eabe85a7f3884fe7
MarketTwits <daniil-kulakov@list.ru>
2022-04-28 19:33:33 +0700

    Add second activity

--- app/src/main/java/com/example/bankactivity/MainActivity.kt
+++ app/src/main/java/com/example/bankactivity/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.bankactivity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.Window
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun onClickActivity(view: View)
+    {
+        val intent = Intent(this, UserProfileActivity::class.java)
+        startActivity(intent)
+
     }
 }
